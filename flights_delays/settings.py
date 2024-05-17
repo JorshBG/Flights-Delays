5026dc34c38870a486d545c08e4b1f8a04e23a5e
JorshBG <jordiyair29@gmail.com>
2024-05-17 08:01:14 -0600

    fix i dont know

--- flights_delays/settings.py
+++ flights_delays/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3=e#*9739)l+c3q^qsyohrob#47cwkukmx!v1qj05uxsyy%c&o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False).lower() == "true"
+DEBUG = os.environ.get('DEBUG', "False").lower() == "true"
 
 # Load environment definition file
 ENV_FILE = find_dotenv()
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
