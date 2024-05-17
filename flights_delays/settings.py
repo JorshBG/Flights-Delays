d08a8101526f297b211dff2e696ef67e0edcf2d4
JorshBG <jordiyair29@gmail.com>
2024-05-17 07:54:08 -0600

    fix middlewares

--- flights_delays/settings.py
+++ flights_delays/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+
+# from django.contrib import staticfiles
 from dotenv import load_dotenv, find_dotenv
 import os
 import dj_database_url
@@ -51,13 +53,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'flights_delays.urls'
